cf8ddbb11304e3bf3b8c36484161856aa76e9ed8
vadim <vamyakish@mail.ru>
2023-12-30 21:57:43 +0300

    8 test alphabet error

--- ssearch/practice/C2.cc
+++ ssearch/practice/C2.cc
@@ -2,25 +2,7 @@
 #include <vector>
 #include <iostream>
 
-// очень наивный вариант решения, который точно не пройдет кучу тестов
-// принцип такой: смотрим на первое ненулевое значение z[i] и в зависимости от
-// него конструируем z[i] элементов строки начиная с текущего (копируем префикс).
-// наивно потому что тут пропускаются все значения z[i] от i до i + z[i] и ориентир
-// идет только на значение z[i]. но может оказаться так что то значение кроторое стоит
-// после текущего, не валидно. нужно как то это учесть. Самое простое решение которое 
-// приходит в голову - сделать валидацию данной z-функции и z-функции, которую составим по готовой строке.
-//
-// проблема: это будет долго, так что должен быть более разумный и быстрый способ это сделать
-// подумаю о нем завтра
-
 int main() {
-
-  /* const char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', */
-  /*                          'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', */
-  /*                          'u', 'v', 'w', 'x', 'w', 'z'}; */
-
-  // small letters 97-122 (26)
-  // big letters 65-90 (26)
 
   int t;
   std::cin >> t;
@@ -37,33 +19,32 @@ int main() {
 
   for (int k = 0; k != t; ++k) {
 
-    int small_begin = 97;
-    /* int big_begin = 65; */
-    std::string s(sz[k], small_begin);
+    int alphabet = 'a';
+    std::string s(sz[k], alphabet);
 
-    if (z[k][0] != 0) s = '!';
+    for (int i = 1; i != sz[k];) {
+      if (z[k][i - 1] > 1) i--;
+      if (z[k][i] > sz[k] - i) break;
 
-    else
-    for (int i = 1; i != sz[k]; ++i) {
-      if (z[k][i] > sz[k] - i) {
-        s = '!'; break;
-      } else if (z[k][i] == 0) {
-        s[i] = ++small_begin;
-
-        if (small_begin == 122) {
-          small_begin = 65;
-        } else if (small_begin == 90 && i < sz[k] - 1) {
-          s = '!';
-          break;
-        }
-
+      if (z[k][i] == 0) {
+          s[i++] = ++alphabet;
+          if (alphabet == 'z')
+            alphabet = 'z' - 1;
       } else {
         for (int j = 0; j < z[k][i]; ++j) {
           s[i + j] = s[j];
         }
-        i += z[k][i] - 1;
+        i += z[k][i];
       }
     }
+
+    std::vector<int> valid_z(sz[k], 0);
+    for (int i = 1; i != sz[k]; ++i) {
+      while (i + valid_z[i] < sz[k] && s[i + valid_z[i]] == s[valid_z[i]])
+        valid_z[i]++;
+    }
+
+    if (z[k] != valid_z) s = '!';
 
     std::cout << s << "\n";
 
